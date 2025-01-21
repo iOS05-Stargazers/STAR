25209a142abe8837e0979e70f5eb862037ac0451
DoyleHWorks <DoyleHWorks@gmail.com>
2025-01-21 21:17:04 +0900

    [Add] 프로젝트 파일 생성

---
+++ star/star/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  star
+//
+//  Created by t0000-m0112 on 2025-01-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
