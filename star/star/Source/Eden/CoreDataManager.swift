bb950b3696d7b61a27f97c11ef80254fb4b53550
Eden <you8inpark@gmail.com>
2025-01-22 22:31:33 +0900

    [Feat] fetchAllStars 메서드 생성

--- star/star/Source/Eden/CoreDataManager.swift
+++ star/star/Source/Eden/CoreDataManager.swift
@@ -16,15 +16,34 @@ class CoreDataManager {
     private var context: NSManagedObjectContext {
         return (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     }
-   
+    
     // Star 생성
-    func createStar() {
-        
+    func createStar(name: String, appList: [String], repeatDays: [String], startTime: String, endTime: String) {
+        let newStar = Star(context: context)
+        newStar.id = UUID()
+        newStar.name = name
+        newStar.appList = appList.joined(separator: ",")
+        newStar.repeatDays = repeatDays.joined(separator: ",")
+        newStar.startTime = startTime
+        newStar.endTime = endTime
+        do {
+            try context.save()
+            print("Star 생성")
+        } catch {
+            print("Star 생성 에러 \(error)")
+        }
     }
     
     // 전체 Star 조회
     func fetchAllStars() {
+        let fetchRequest: NSFetchRequest<Star> = Star.fetchRequest()
         
+        do {
+            let stars = try context.fetch(fetchRequest)
+            print("Star 조회: \(stars)")
+        } catch {
+            print("Star 조회 에러 \(error)")
+        }
     }
     
     // Star 수정
