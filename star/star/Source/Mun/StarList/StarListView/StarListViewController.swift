f3b2c13688efea4804e7df1ad2d3fe06b2946a25
name-mun <tains9153@gmail.com>
2025-01-27 11:55:24 +0900

    [Refactor] 버튼, 셀 이벤트 Rx로 바인딩

--- star/star/Source/Mun/StarList/StarListView/StarListViewController.swift
+++ star/star/Source/Mun/StarList/StarListView/StarListViewController.swift
@@ -23,7 +23,7 @@ class StarListViewController: UIViewController {
     override func loadView() {
         view = starListView
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         bind()
@@ -31,27 +31,48 @@ class StarListViewController: UIViewController {
     }
 }
 
+// MARK: - bind
+
 extension StarListViewController {
-    // bind
     private func bind() {
         let viewWillAppears = rx.methodInvoked(#selector(viewWillAppear)).map { _ in }
         let input = StarListViewModel.Input(viewWillAppear: viewWillAppears)
         let output = viewModel.transform(input)
         
+        // 컬렉션뷰 데이터 바인딩
         output.starDataSource
             .drive(starListView.starListCollectionView.rx.items(
                 cellIdentifier: StarListCollectionViewCell.id,
                 cellType: StarListCollectionViewCell.self)) { row, element, cell in
                     cell.titleLabel.text = element.title
-            }
-            .disposed(by: disposeBag)
+                }
+                .disposed(by: disposeBag)
         
+        // 오늘 날짜 데이터 바인딩
         output.date
             .map { TodayDate.formatDate(date: $0)}
             .drive(starListView.todayDateLabel.rx.text)
             .disposed(by: disposeBag)
+        
+        // 추가하기 버튼 이벤트 처리
+        starListView.addStarButton.rx.tap
+            .asDriver()
+            .drive(onNext: { [weak self] in
+                self?.connectCreateModal()
+            })
+            .disposed(by: disposeBag)
+        
+        
+        // 셀 선택 이벤트 처리
+        starListView.starListCollectionView.rx.itemSelected
+            .subscribe(onNext: { _ in
+                print("셀 클릭")
+            })
+            .disposed(by: disposeBag)
     }
 }
+
+// MARK: - 실행 메서드
 
 extension StarListViewController {
     // 스와이프 액션 설정
@@ -75,10 +96,26 @@ extension StarListViewController {
     }
     
     // 삭제하기 알럿 띄우기
-       private func showAlert() {
-           let starDeleteAlertViewController = StarDeleteAlertViewController()
-           starDeleteAlertViewController.modalPresentationStyle = .overFullScreen
-           starDeleteAlertViewController.view.backgroundColor = UIColor.black.withAlphaComponent(0.65)
-           present(starDeleteAlertViewController, animated: true)
-       }
+    private func showAlert() {
+        let starDeleteAlertViewController = StarDeleteAlertViewController()
+        starDeleteAlertViewController.modalPresentationStyle = .overFullScreen
+        starDeleteAlertViewController.view.backgroundColor = UIColor.black.withAlphaComponent(0.65)
+        present(starDeleteAlertViewController, animated: true)
+    }
+    
+    // 생성하기 모달 연결
+    private func connectCreateModal() {
+        let modalVC = StarModalViewController()
+        modalVC.sheetPresentationController?.detents = [.custom(resolver: { context in
+            let modalHeight = UIScreen.main.bounds.size.height - self.starListView.topView.frame.maxY - self.view.safeAreaInsets.bottom - 4
+            return modalHeight
+        })
+        ]
+        modalVC.sheetPresentationController?.selectedDetentIdentifier = .medium
+        modalVC.sheetPresentationController?.prefersGrabberVisible = true
+        modalVC.modalPresentationStyle = .formSheet
+        modalVC.view.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        modalVC.view.layer.cornerRadius = 40
+        present(modalVC, animated: true)
+    }
 }
