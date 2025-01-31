c3b57b2ed349c505b647c1710535c36544a7b0ff
Eden <you8inpark@gmail.com>
2025-01-31 13:24:00 +0900

    [Feat] PermissionViewModel 생성

---
+++ star/star/Source/Eden/PermissionViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  PermissionViewModel.swift
+//  star
+//
+//  Created by Eden on 1/31/25.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class PermissionViewModel {
+    private let disposeBag = DisposeBag()
+
+    init() {}
+
+}
+
+extension PermissionViewModel {
+
+    struct Input {
+
+    }
+
+    struct Output {
+
+    }
+
+}
