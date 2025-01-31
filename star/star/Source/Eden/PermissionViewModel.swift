44a33de501f9a3086fc69746e1303fc58b9512db
Eden <you8inpark@gmail.com>
2025-01-31 14:59:28 +0900

    [Refactor] Input Output 관리하는 extension PermissionViewModel 생성

--- star/star/Source/Eden/PermissionViewModel.swift
+++ star/star/Source/Eden/PermissionViewModel.swift
@@ -11,19 +11,23 @@ import RxCocoa
 
 final class PermissionViewModel {
     private let disposeBag = DisposeBag()
-
+    
     init() {}
-
+    
 }
 
 extension PermissionViewModel {
-
+    
+    // MARK: - Input
+    
     struct Input {
-
+        let requestPermissionTrigger: Observable<Void>
     }
-
+    
+    // MARK: - Output
+    
     struct Output {
-
+        let isPermissionGranted: Observable<Bool>
+        let navigateToStarList: Observable<Void>
     }
-
 }
