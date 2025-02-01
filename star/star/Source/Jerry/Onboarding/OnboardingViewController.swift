76463d805464a66d490c7db6c5342eaa0528766d
이재영 <ldc1000@naver.com>
2025-02-01 15:39:57 +0900

    [Feat] OnboardingVIew 구현

---
+++ star/star/Source/Jerry/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,37 @@
+//
+//  OnboardingViewController.swift
+//  star
+//
+//  Created by 0-jerry on 1/31/25.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class OnboardingViewController: UIViewController {
+
+    override func loadView() {
+        view = OnboardingView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .starDisabledTagBG.withAlphaComponent(0.8)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.isNavigationBarHidden = false
+    }
+    // 화면 터치 시 모달 내림
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        UserDefaults.standard.isCoachMarkShown = true
+        dismiss(animated: false)
+    }
+}
