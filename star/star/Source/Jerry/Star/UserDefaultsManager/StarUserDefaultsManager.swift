2878e050f256c3425cd73e988114cd147a936644
이재영 <ldc1000@naver.com>
2025-02-04 14:54:23 +0900

    [Refactor] StarIDFormatter, StarIDManager 를 통한 메서드 분리

--- star/star/Source/Jerry/Star/UserDefaultsManager/StarUserDefaultsManager.swift
+++ star/star/Source/Jerry/Star/UserDefaultsManager/StarUserDefaultsManager.swift
@@ -18,11 +18,12 @@ final class StarUserDefaultsManager {
     
     private init(_ userDefaults: UserDefaults = UserDefaults.standard) {
         self.userDefaults = userDefaults
+        self.starIDManager = StarIDManager(userDefaults: userDefaults)
     }
     
-    private let header = "[STAR]"
-    
     private let userDefaults: UserDefaults
+    
+    private let starIDManager: StarIDManager
     
     func create(_ star: Star) {
         guard let data = star.jsonData() else {
@@ -31,12 +32,12 @@ final class StarUserDefaultsManager {
         }
         
         let id = star.identifier
-        appendID(id)
-        userDefaults.set(data, forKey: key(id))
+        starIDManager.appendID(id)
+        userDefaults.set(data, forKey: StarIDFormatter.key(id))
     }
     
     func read() -> [Star] {
-        return identifiers().compactMap { star($0) }
+        return starIDManager.starIDList.compactMap { star($0) }
     }
     
     func update(_ star: Star) {
@@ -45,56 +46,28 @@ final class StarUserDefaultsManager {
             print("\(star.title) - 데이터 저장 실패")
             return }
         
-        userDefaults.set(data, forKey: key(id))
+        userDefaults.set(data, forKey: StarIDFormatter.key(id))
     }
     
     func delete(_ star: Star) {
         let id = star.identifier
-        removeID(id)
-        userDefaults.set(nil, forKey: key(id))
+        starIDManager.removeID(id)
+        userDefaults.set(nil, forKey: StarIDFormatter.key(id))
     }
     // 전체 삭제
     func reset() {
-        identifiers().forEach { delete(id: $0) }
+        starIDManager.starIDList.forEach { delete(id: $0) }
     }
-    
-    func printWhole() {
-        var description = read().map { $0.testDescription }.joined(separator: "\n\n")
-        description = description != "" ? description : "데이터 없음"
-        print("\n\(description)\n")
-    }
-    
-    func test() {
-        print("======CREATE TEST======")
-        self.create(MockData.ongingOneHour)
-        self.create(MockData.pendingOneDay)
-        self.printWhole()
-        print("=======================\n\n\n")
 
-        print("======DELETE TEST======")
-        self.delete(MockData.ongingOneHour)
-        self.delete(MockData.pendingOneDay)
-        self.printWhole()
-        print("=======================\n\n\n")
-        
-        print("======UPDATE TEST======")
-        self.create(MockData.beforeUpdate)
-        self.printWhole()
-        self.update(MockData.afterUpdate)
-        self.printWhole()
-        print("=======================\n\n\n")
 
-        self.reset()
-    }
-    
     private func delete(id: UUID) {
         guard let star = star(id) else { return }
         delete(star)
-        removeID(id)
+        starIDManager.removeID(id)
     }
     
     private func star(_ id: UUID) -> Star? {
-        guard let data = userDefaults.data(forKey: key(id)),
+        guard let data = userDefaults.data(forKey: StarIDFormatter.key(id)),
               let star = Star(from: data) else { return nil }
         
         return star
@@ -102,43 +75,37 @@ final class StarUserDefaultsManager {
     
 }
 
-// MARK: - ID 관련 메서드
+struct StarUserDefaultsManagerTester {
+    
+    let manager = StarUserDefaultsManager.shared
+    
+    func test() {
+        
+        print("======CREATE TEST======")
+        manager.create(MockData.ongingOneHour)
+        manager.create(MockData.pendingOneDay)
+        printWhole()
+        print("=======================\n\n\n")
 
-extension StarUserDefaultsManager {
-    // ID 목록 추가하기
-    private func appendID(_ id: UUID) {
-        var identifiers = identifiers()
-        identifiers.append(id)
-        let keys = identifiers.map { key($0) }
+        print("======DELETE TEST======")
+        manager.delete(MockData.ongingOneHour)
+        manager.delete(MockData.pendingOneDay)
+        printWhole()
+        print("=======================\n\n\n")
         
-        userDefaults.setValue(keys, forKey: Key.identifiers)
-    }
-    // ID 목록 제거하기
-    private func removeID(_ id: UUID) {
-        let identifiers = identifiers()
-            .filter { $0 != id }
-            .map { key($0) }
-        
-        userDefaults.setValue(identifiers, forKey: Key.identifiers)
-    }
-    // ID 목록 불러오기
-    private func identifiers() -> [UUID] {
-        let stringIDs = (userDefaults.stringArray(forKey: Key.identifiers) ?? []) as [String]
-        return stringIDs.compactMap { uuid($0) }
-    }
-    // UUID -> key(String) 변환
-    private func key(_ id: UUID) -> String {
-        let uuidString = id.uuidString
-        let key = String(format: "%@:%@", header, uuidString)
-        return key
+        print("======UPDATE TEST======")
+        manager.create(MockData.beforeUpdate)
+        printWhole()
+        manager.update(MockData.afterUpdate)
+        printWhole()
+        print("=======================\n\n\n")
+
+        manager.reset()
     }
     
-    private func uuid(_ key: String) -> UUID? {
-        let components = key.components(separatedBy: ":")
-        guard components.count == 2,
-              components[0] == header,
-              let uuid = UUID(uuidString: components[1]) else { return nil }
-        
-        return uuid
+    private func printWhole() {
+        var description = manager.read().map { $0.testDescription }.joined(separator: "\n\n")
+        description = description != "" ? description : "데이터 없음"
+        print("\n\(description)\n")
     }
 }
