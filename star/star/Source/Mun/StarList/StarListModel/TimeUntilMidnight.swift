0ea0628a6391adfca4b2d90dc39483963501bf7d
name-mun <tains9153@gmail.com>
2025-02-05 13:16:24 +0900

    [Refactor] 개행 수정

--- star/star/Source/Mun/StarList/StarListModel/TimeUntilMidnight.swift
+++ star/star/Source/Mun/StarList/StarListModel/TimeUntilMidnight.swift
@@ -16,7 +16,14 @@ struct TimeUntilMidnight {
         let month = calendar.component(.month, from: now)
         let day = calendar.component(.day, from: now)
 
-        let tomorrow = calendar.date(from: DateComponents(year: year, month: month, day: day + 1, hour: 0, minute: 0, second: 0)) // 다음 날 자정
+        let tomorrow = calendar.date(from: DateComponents(
+            year: year,
+            month: month,
+            day: day + 1,
+            hour: 0,
+            minute: 0,
+            second: 0
+        )) // 다음 날 자정
         return tomorrow?.timeIntervalSince(now) // 다음 날 자정까지 남은 시간 반환
     }
 }
