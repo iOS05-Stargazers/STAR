7cd28425ac95515e7859881492741731006eb084
Eden <you8inpark@gmail.com>
2025-02-06 07:52:54 +0900

    [Feat] RestingViewController 생성

---
+++ star/star/Source/Eden/RestingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RestingViewController.swift
+//  star
+//
+//  Created by Eden on 2/6/25.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class RestingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+}
