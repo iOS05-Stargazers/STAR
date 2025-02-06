aee164ec36c078f467e04753ceeaf142713ee86e
Eden <you8inpark@gmail.com>
2025-02-06 13:13:32 +0900

    [Feat] 휴식중 titleLabel, timerLabel 생성 및 레이아웃 정의

--- star/star/Source/Eden/RestingViewController.swift
+++ star/star/Source/Eden/RestingViewController.swift
@@ -11,9 +11,42 @@ import Then
 
 final class RestingViewController: UIViewController {
     
+    // MARK: - UI Components
+    
+    private let titleLabel = UILabel().then {
+        $0.text = "휴식중"
+        $0.font = .boldSystemFont(ofSize: 32)
+        $0.textColor = .starPrimaryText
+    }
+    
+    private let timerLabel = UILabel().then {
+        $0.text = "00:00:00"
+        $0.font = .monospacedDigitSystemFont(ofSize: 32, weight: .bold)
+        $0.textColor = .starSecondaryText
+    }
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+    }
+    
+    // MARK: - Set Up
+    
+    private func setupUI() {
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.7)
         
-        view.backgroundColor = .systemBackground
+        view.addSubviews(titleLabel, timerLabel)
+        
+        titleLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview().offset(-40)
+        }
+        
+        timerLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(titleLabel.snp.bottom).offset(8)
+        }
     }
 }
--- star/star/Source/Mun/StarList/StarListView/StarListViewController.swift
+++ star/star/Source/Mun/StarList/StarListView/StarListViewController.swift
@@ -36,13 +36,19 @@ final class StarListViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        // 온보딩 뷰를 보여주지 않았다면 온보딩뷰 표시
+
+        // 온보딩 뷰를 보여주지 않았다면 온보딩 뷰 표시
         if !UserDefaults.standard.isCoachMarkShown {
             let onboardingViewController = OnboardingViewController()
             onboardingViewController.modalPresentationStyle = .overFullScreen
             present(onboardingViewController, animated: false)
+        } else {
+            let restingVC = RestingViewController()
+            restingVC.modalPresentationStyle = .overFullScreen
+            present(restingVC, animated: false)
         }
     }
+
 }
 
 // MARK: - bind
