0bb683113526694eebbaf01f371c8d44a0907f41
Eden <you8inpark@gmail.com>
2025-02-06 17:17:51 +0900

    [Feat] 휴식 종료하기 버튼 생성

--- star/star/Source/Eden/RestingViewController.swift
+++ star/star/Source/Eden/RestingViewController.swift
@@ -16,7 +16,7 @@ final class RestingViewController: UIViewController {
     private let titleLabel = UILabel().then {
         $0.text = "휴식중"
         $0.font = .boldSystemFont(ofSize: 32)
-        $0.textColor = .starPrimaryText
+        $0.textColor = .starSecondaryText
     }
     
     private let timerLabel = UILabel().then {
@@ -24,6 +24,17 @@ final class RestingViewController: UIViewController {
         $0.font = .monospacedDigitSystemFont(ofSize: 32, weight: .bold)
         $0.textColor = .starSecondaryText
     }
+    
+    private let endRestButton = GradientButton().then {
+        $0.setTitle("휴식 종료하기", for: .normal)
+        $0.titleLabel?.font = .boldSystemFont(ofSize: 18)
+        $0.setTitleColor(.starTertiaryText, for: .normal)
+        $0.layer.cornerRadius = 24
+        $0.clipsToBounds = true
+        
+        $0.applyGradient(colors: [.starButtonWhite, .starButtonYellow], direction: .horizontal)
+    }
+
     
     // MARK: - Life Cycle
     
@@ -35,9 +46,9 @@ final class RestingViewController: UIViewController {
     // MARK: - Set Up
     
     private func setupUI() {
-        view.backgroundColor = UIColor.black.withAlphaComponent(0.7)
+        view.backgroundColor = .starModalOverlayBG
         
-        view.addSubviews(titleLabel, timerLabel)
+        view.addSubviews(titleLabel, timerLabel, endRestButton)
         
         titleLabel.snp.makeConstraints {
             $0.centerX.equalToSuperview()
@@ -47,6 +58,13 @@ final class RestingViewController: UIViewController {
         timerLabel.snp.makeConstraints {
             $0.centerX.equalToSuperview()
             $0.top.equalTo(titleLabel.snp.bottom).offset(8)
+        }
+        
+        endRestButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).inset(20)
+            $0.height.equalTo(56)
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
     }
 }
