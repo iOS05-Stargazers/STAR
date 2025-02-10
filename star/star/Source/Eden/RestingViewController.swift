32aa6a432c38510e6b750333c8534ea7a2535f7a
Eden <you8inpark@gmail.com>
2025-02-10 14:20:58 +0900

    [Feat] RestingViewModel 생성 후 RestingViewController에 bind

--- star/star/Source/Eden/RestingViewController.swift
+++ star/star/Source/Eden/RestingViewController.swift
@@ -8,8 +8,20 @@
 import UIKit
 import SnapKit
 import Then
+import RxSwift
+import RxCocoa
 
 final class RestingViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let viewModel = RestingViewModel()
+    private let disposeBag = DisposeBag()
+    
+    private let startTimerSubject = PublishSubject<Void>()
+    private let stopTimerSubject = PublishSubject<Void>()
+    
+    private let initialTime: Int
     
     // MARK: - UI Components
     
@@ -34,13 +46,26 @@ final class RestingViewController: UIViewController {
         
         $0.applyGradient(colors: [.starButtonWhite, .starButtonYellow], direction: .horizontal)
     }
-
+    
+    // MARK: - Init
+    
+    init(initialTime: Int = 300) {
+        self.initialTime = initialTime
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        bind()
+        
+        startTimerSubject.onNext(())
     }
     
     // MARK: - Set Up
@@ -67,4 +92,28 @@ final class RestingViewController: UIViewController {
             $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
     }
+    
+    // MARK: - Bind ViewModel
+    
+    private func bind() {
+        let input = RestingViewModel.Input(
+            startTimer: startTimerSubject.asObservable(),
+            stopTimer: endRestButton.rx.tap.asObservable()
+        )
+        
+        let output = viewModel.transform(input: input, initialTime: initialTime)
+        
+        // 타이머 값 변경 시 `timerLabel` 업데이트
+        output.timerText
+            .drive(timerLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        // 종료 버튼 클릭 시 모달 닫기
+        endRestButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                self?.dismiss(animated: true)
+            })
+            .disposed(by: disposeBag)
+    }
 }
+
--- star/star/Source/Eden/RestingViewModel.swift
+++ star/star/Source/Eden/RestingViewModel.swift
@@ -11,18 +11,50 @@ import RxCocoa
 
 final class RestingViewModel {
     
+    private let disposeBag = DisposeBag()
 }
 
 extension RestingViewModel {
+    
     struct Input {
-        
+        let startTimer: Observable<Void>
+        let stopTimer: Observable<Void>
     }
     
     struct Output {
-        
+        let timerText: Driver<String>
     }
     
-    func transform(input: Input) -> Output {
-        return Output()
+    func transform(input: Input, initialTime: Int) -> Output {
+        let timerSubject = BehaviorRelay<Int>(value: initialTime)
+        
+        input.startTimer
+            .flatMapLatest { _ in
+                Observable<Int>.interval(.seconds(1), scheduler: MainScheduler.instance)
+                    .map { _ in -1 } // 1초마다 -1 감소
+            }
+            .withLatestFrom(timerSubject) { decrement, current in
+                max(current + decrement, 0) // 0 이하로 내려가지 않도록
+            }
+            .distinctUntilChanged()
+            .bind(to: timerSubject)
+            .disposed(by: disposeBag)
+        
+        input.stopTimer
+            .subscribe(onNext: { _ in
+                timerSubject.accept(0)
+            })
+            .disposed(by: disposeBag)
+        
+        let timerText = timerSubject
+            .map { seconds -> String in
+                let hours = seconds / 3600
+                let minutes = (seconds % 3600) / 60
+                let secs = seconds % 60
+                return String(format: "%02d:%02d:%02d", hours, minutes, secs)
+            }
+            .asDriver(onErrorJustReturn: "00:00:00")
+        
+        return Output(timerText: timerText)
     }
 }
