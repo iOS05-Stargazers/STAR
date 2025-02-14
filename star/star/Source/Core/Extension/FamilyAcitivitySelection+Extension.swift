dcb37e7bd838e71743356142b30193ddcfb9faf9
이재영 <ldc1000@naver.com>
2025-02-14 15:27:06 +0900

    [Feat] FamilyActivitySelection - isEmpty 추가

--- star/star/Source/Core/Extension/FamilyAcitivitySelection+Extension.swift
+++ star/star/Source/Core/Extension/FamilyAcitivitySelection+Extension.swift
@@ -26,6 +26,16 @@ extension FamilyActivitySelection: RawRepresentable {
     }
 }
 
+extension FamilyActivitySelection {
+    // 토큰이 비어있는지 확인
+    var isEmpty: Bool {
+        self.applicationTokens.isEmpty &&
+        self.categoryTokens.isEmpty &&
+        self.webDomainTokens.isEmpty
+    }
+    
+}
+
 // 데이터 리프레쉬
 extension FamilyActivitySelection {
     
