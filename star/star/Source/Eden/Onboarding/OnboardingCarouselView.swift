e5d8b4cad0f5529b03f5912f150059304486f09b
Eden <you8inpark@gmail.com>
2025-02-14 23:55:00 +0900

    [Feat] OnboardingCarouselView 생성

---
+++ star/star/Source/Eden/Onboarding/OnboardingCarouselView.swift
@@ -0,0 +1,23 @@
+//
+//  OnboardingCarouselView.swift
+//  star
+//
+//  Created by Eden on 2/14/25.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class OnboardingCarouselView: UIView, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+}
