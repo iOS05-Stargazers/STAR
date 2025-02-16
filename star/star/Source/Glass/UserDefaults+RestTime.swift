850ac5b418b4bdf541eb1673c511c87429a87f7a
이재영 <ldc1000@naver.com>
2025-02-16 16:23:26 +0900

    [Fix] UserDefaults+RestTime 수정

--- star/star/Source/Glass/UserDefaults+RestTime.swift
+++ star/star/Source/Glass/UserDefaults+RestTime.swift
@@ -12,20 +12,20 @@ extension UserDefaults {
     // CREATE/UPDATE
     func restEndTimeSet(_ value: Int) -> Date? {
         let endTime = endTimeCalculate(minutes: value)
-        UserDefaults.standard.set(endTime, forKey: "restEndTime")
+        self.set(endTime, forKey: "restEndTime")
         
         return endTime
     }
     
     // READ
     func restEndTimeGet() -> Date? {
-        guard let endTime = UserDefaults.standard.value(forKey: "restEndTime") as? Date else { return nil }
+        guard let endTime = self.value(forKey: "restEndTime") as? Date else { return nil }
         return endTime
     }
     
     // DELETE
     func restEndTimeDelete() {
-        UserDefaults.standard.removeObject(forKey: "restEndTime")
+        self.removeObject(forKey: "restEndTime")
     }
     
     // 휴식 종료시간 계산
