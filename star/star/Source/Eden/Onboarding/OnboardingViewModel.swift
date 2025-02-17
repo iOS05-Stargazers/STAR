d22a414d79edb1f7ceaf98c49881c8a9994aba5a
Eden <you8inpark@gmail.com>
2025-02-17 14:59:47 +0900

    [Refactor] OnboardingViewModel을 Model에 맞게 변경

--- star/star/Source/Eden/Onboarding/OnboardingViewModel.swift
+++ star/star/Source/Eden/Onboarding/OnboardingViewModel.swift
@@ -14,53 +14,19 @@ final class OnboardingViewModel {
     
     let pages: [OnboardingModel] = [
         OnboardingModel(
-            highlightElements: [
-                OnboardingHighlightElement(
-                    image: UIImage(named: "starButtonImage"),
-                    position: (xMultiplier: 0.5, yMultiplier: 1.2),
-                    leadingInset: 20,
-                    trailingInset: 20
-                )
-            ],
+            image: UIImage(named: "onboardingCreateImage"),
             description: "\"스타 추가하기\"를 통해\n스타를 생성할 수 있습니다."
         ),
         OnboardingModel(
-            highlightElements: [
-                OnboardingHighlightElement(
-                    image: UIImage(named: "starCellImage"),
-                    position: (xMultiplier: 0.5, yMultiplier: 0.75),
-                    leadingInset: 20,
-                    trailingInset: 20
-                )
-            ],
+            image: UIImage(named: "onboardingModifyImage"),
             description: "목록의 스타를 선택하면\n스타를 수정할 수 있습니다."
         ),
         OnboardingModel(
-            highlightElements: [
-                OnboardingHighlightElement(
-                    image: UIImage(named: "starCellImage"),
-                    position: (xMultiplier: 0.5, yMultiplier: 0.75),
-                    leadingInset: 20,
-                    trailingInset: 20
-                ),
-                OnboardingHighlightElement(
-                    image: UIImage(systemName: "hand.draw.fill"),
-                    position: (xMultiplier: 0.5, yMultiplier: 0.85),
-                    leadingInset: 0,
-                    trailingInset: 0
-                )
-            ],
+            image: UIImage(named: "onboardingDeleteImage"),
             description: "왼쪽으로 스와이프하여\n스타를 삭제할 수 있습니다."
         ),
         OnboardingModel(
-            highlightElements: [
-                OnboardingHighlightElement(
-                    image: UIImage(systemName: "hand.tap.fill"),
-                    position: (xMultiplier: 0.7, yMultiplier: 0.2),
-                    leadingInset: 0,
-                    trailingInset: 0
-                )
-            ],
+            image: UIImage(named: "onboardingRestImage"),
             description: "휴식 버튼을 누르면\n휴식 모드로 전환할 수 있습니다."
         )
     ]
