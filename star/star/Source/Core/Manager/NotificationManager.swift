d19a40e48795cf06b0c188b91e2ffbfcab757d78
name-mun <tains9153@gmail.com>
2025-02-18 21:45:23 +0900

    [Feat] 5분 전 알림 기능 구현

--- star/star/Source/Core/Manager/NotificationManager.swift
+++ star/star/Source/Core/Manager/NotificationManager.swift
@@ -10,32 +10,32 @@ import UserNotifications
 
 enum NotificationType {
     
-    case startTime(star: Star)
-    case endTime(star: Star)
+    case willStartSoon(star: Star)
+    case didStart(star: Star)
+    case didEnd(star: Star)
     
     var title: String {
-        switch self {
-        case .startTime:
-            return "스타 시작 알림"
-        case .endTime:
-            return "스타 종료 알림"
-        }
+        return "STAR"
     }
     
     var body: String {
         switch self {
-        case .startTime(let star):
+        case .willStartSoon(let star):
+            return "\(star.title) 스타가 5분 뒤에 시작됩니다."
+        case .didStart(let star):
             return "\(star.title) 스타가 시작되었습니다."
-        case .endTime(let star):
+        case .didEnd(let star):
             return "\(star.title) 스타가 종료되었습니다."
         }
     }
     
     var identifier: String {
         switch self {
-        case .startTime(let star):
+        case .willStartSoon(let star):
+            return "\(star.identifier.uuidString)_startBefore"
+        case .didStart(let star):
             return "\(star.identifier.uuidString)_start"
-        case .endTime(let star):
+        case .didEnd(let star):
             return "\(star.identifier.uuidString)_finish"
         }
     }
@@ -51,36 +51,28 @@ final class NotificationManager: NSObject {
     
     // 알림 생성 (일정 등록)
     func scheduleNotificaions(star: Star) {
-        let startType = NotificationType.startTime(star: star)
-        let endType = NotificationType.endTime(star: star)
-        
-        let startTimeContent = buildNotificationContent(mode: startType)
-        let endTimeContent = buildNotificationContent(mode: endType)
-        
-        let startTimes = star.schedule.starTimeDateComponentsList()
-        let endTimes = star.schedule.finishTimeDateComponentsList()
-        
-        startTimes.forEach {
-            scheduleNotification(
-                dateComponents: $0,
-                type: startType,
-                content: startTimeContent
-            )
-        }
-        
-        endTimes.forEach {
-            scheduleNotification(
-                dateComponents: $0,
-                type: endType,
-                content: endTimeContent
-            )
-        }
+        scheduleNotificationList(
+            for: star,
+            type: .willStartSoon(star: star),
+            dates: calculateWillStartSoonTimes(star)
+        )
+        scheduleNotificationList(
+            for: star,
+            type: .didStart(star: star),
+            dates: star.schedule.starTimeDateComponentsList()
+        )
+        scheduleNotificationList(
+            for: star,
+            type: .didEnd(star: star),
+            dates: star.schedule.finishTimeDateComponentsList()
+        )
     }
     
     // 알림 삭제 (예약 취소)
     func cancelNotification(star: Star) {
-        let startId = NotificationType.startTime(star: star).identifier
-        let finishId = NotificationType.endTime(star: star).identifier
+        let startBeforeId = NotificationType.willStartSoon(star: star).identifier
+        let startId = NotificationType.didStart(star: star).identifier
+        let finishId = NotificationType.didEnd(star: star).identifier
         
         // 예약된 알림 삭제
         UNUserNotificationCenter.current()
@@ -114,6 +106,39 @@ final class NotificationManager: NSObject {
         
         // 알림 등록
         UNUserNotificationCenter.current().add(request) { _ in }
+    }
+    
+    // 특정 타입의 알림을 여러 개 스케줄링 하는 헬퍼 메서드
+    private func scheduleNotificationList(for star: Star, type: NotificationType, dates: [DateComponents]) {
+        let content = buildNotificationContent(mode: type)
+        
+        dates.forEach {
+            scheduleNotification(
+                dateComponents: $0,
+                type: type,
+                content: content
+            )
+        }
+    }
+    
+    // 5분 전 알림 시간 계산
+    private func calculateWillStartSoonTimes(_ star: Star) -> [DateComponents] {
+        let startTime = star.schedule.startTime
+
+        let willStartSoonList: [DateComponents] = star.schedule.weekDays.map { WeekDay in
+            let startDate = DateComponents(hour: startTime.hour,
+                                           minute: startTime.minute,
+                                           weekday: WeekDay.rawValue)
+            
+            let calender = Calendar.current
+            if let newDate = calender.date(from: startDate)?.addingTimeInterval(-5 * 60) {
+                let newComponenets = calender.dateComponents([.hour, .minute, .weekday], from: newDate)
+                return newComponenets
+            }
+            
+            return startDate
+        }
+        return willStartSoonList
     }
 }
 
