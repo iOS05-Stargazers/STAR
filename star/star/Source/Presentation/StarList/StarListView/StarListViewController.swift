6e6cf8be00d3c817f08ac450a26bbf3b0acf9739
name-mun <tains9153@gmail.com>
2025-02-18 21:09:22 +0900

    [Refactor] 메서드명 변경

--- star/star/Source/Presentation/StarList/StarListView/StarListViewController.swift
+++ star/star/Source/Presentation/StarList/StarListView/StarListViewController.swift
@@ -131,7 +131,7 @@ extension StarListViewController {
     private func connectModal(_ modal: StarModalState) {
         switch modal {
         case .onboarding:
-            connnectOnboarding()
+            connectOnboarding()
         case .restSetting:
             connectRestSettingModal()
         case .restStart:
@@ -151,7 +151,7 @@ extension StarListViewController {
     }
     
     // 온보딩 모달 연결
-    private func connnectOnboarding() {
+    private func connectOnboarding() {
         let onboardingViewController = OnboardingViewController()
         onboardingViewController.modalPresentationStyle = .overFullScreen
         present(onboardingViewController, animated: false)
