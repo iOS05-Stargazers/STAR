997bd4862d5f2722bd93b5bd625aff9b1b9e8926
Eden <you8inpark@gmail.com>
2025-02-19 22:40:34 +0900

    [Refactor] "STAR" 텍스트 자간과 글씨체 변경 적용 위해 UILabel+Extension 리팩토링

--- star/star/Source/Core/Extension/UILabel+Extension.swift
+++ star/star/Source/Core/Extension/UILabel+Extension.swift
@@ -5,22 +5,26 @@
 //  Created by Eden on 1/26/25.
 //
 
-import UIKit
+import UIKit.UILabel
 
 extension UILabel {
-    /// 로고 글자 대치 후 글씨체 변경하는  extension
-    func setStarHighlightedText(fullText: String, font: UIFont, color: UIColor) {
-        let attributedString = NSMutableAttributedString(string: fullText)
-        attributedString.addAttribute(.font, value: font, range: NSRange(location: 0, length: fullText.count))
-        attributedString.addAttribute(.foregroundColor, value: color, range: NSRange(location: 0, length: fullText.count))
+    /// localized된 텍스트를 설정하면서 "STAR"를 "S T A R"로 변환 후 폰트 변경 적용
+    func setLocalizedText(_ key: String, font: UIFont, color: UIColor) {
+        let localizedText = key.localized
+        let transformedText = localizedText.replacingOccurrences(of: "STAR", with: "S T A R")
+        let attributedString = NSMutableAttributedString(string: transformedText)
         
-        if let starRange = fullText.range(of: "S T A R") {
-            let nsRange = NSRange(starRange, in: fullText)
+        /// 기본 폰트 & 색상 적용
+        attributedString.addAttribute(.font, value: font, range: NSRange(location: 0, length: transformedText.count))
+        attributedString.addAttribute(.foregroundColor, value: color, range: NSRange(location: 0, length: transformedText.count))
+        
+        /// "S T A R" 단어가 포함되어 있다면, 별도 폰트 적용
+        if let starRange = transformedText.range(of: "S T A R") {
+            let nsRange = NSRange(starRange, in: transformedText)
             
             if let customFont = UIFont(name: "SEBANGGothicOTFBold", size: font.pointSize) {
                 attributedString.addAttribute(.font, value: customFont, range: nsRange)
             }
-            attributedString.addAttribute(.foregroundColor, value: color, range: nsRange)
         }
         
         self.attributedText = attributedString
