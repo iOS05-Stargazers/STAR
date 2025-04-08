d25508d663afcaf1a58a9aef8a7d3e9e84287f50
이재영 <ldc1000@naver.com>
2025-04-08 17:19:48 +0900

    [Refactor] 미적용 코드 삭제 ( 권한요청 뷰 - STAR 글씨 강조 메서드 )

--- star/star/Source/Core/Extension/UILabel+Extension.swift
+++ star/star/Source/Core/Extension/UILabel+Extension.swift
@@ -8,21 +8,5 @@
 import UIKit.UILabel
 
 extension UILabel {
-    /// S T A R 텍스트를 자동으로 강조
-    func setStarHighlightedText(fullText: String, font: UIFont, color: UIColor) {
-        let attributedString = NSMutableAttributedString(string: fullText)
-        attributedString.addAttribute(.font, value: font, range: NSRange(location: 0, length: fullText.count))
-        attributedString.addAttribute(.foregroundColor, value: color, range: NSRange(location: 0, length: fullText.count))
-        
-        if let starRange = fullText.range(of: "S T A R") {
-            let nsRange = NSRange(starRange, in: fullText)
-            
-            if let customFont = UIFont(name: "SEBANGGothicOTFBold", size: font.pointSize) {
-                attributedString.addAttribute(.font, value: customFont, range: nsRange)
-            }
-            attributedString.addAttribute(.foregroundColor, value: color, range: nsRange)
-        }
-        
-        self.attributedText = attributedString
-    }
+   
 }
