af4b5a9deb62f0a6ae39f64fd429b91ebd357d84
이재영 <ldc1000@naver.com>
2025-04-09 09:41:09 +0900

    [Chore] 주석 추가

--- star/star/Source/Core/Extension/ScreenTime/ManagedSettings/ShieldSettings+Extension.swift
+++ star/star/Source/Core/Extension/ScreenTime/ManagedSettings/ShieldSettings+Extension.swift
@@ -9,13 +9,13 @@ import FamilyControls
 import ManagedSettings
 
 extension ShieldSettings {
-    
+    // 블록 리스트 설정
     mutating func setBlockList(_ familyActivitySelection: FamilyActivitySelection) {
         applicationCategories = .specific(familyActivitySelection.categoryTokens)
         applications = familyActivitySelection.applicationTokens
         webDomains = familyActivitySelection.webDomainTokens
     }
-    
+    // 블록 리스트 삭제
     mutating func clearBlockList() {
         applicationCategories = .specific([])
         applications = []
