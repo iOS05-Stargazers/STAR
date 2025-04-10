ddc8e7f17c4fd3ebeb518c2279df56525b23ee23
이재영 <ldc1000@naver.com>
2025-04-11 06:28:02 +0900

    [Feat] 중단된 스타에 대한 수정/삭제 에 대한 코드 추가

--- star/star/Source/Core/Manager/StarManager/StarManager.swift
+++ star/star/Source/Core/Manager/StarManager/StarManager.swift
@@ -15,6 +15,7 @@ final class StarManager {
     private let userDefaultsManager = StarUserDefaultsManager.shared
     private let deviceActivityScheduleManager = DeviceActivityScheduleManager()
     private let managedSettingsStoreManager = ManagedSettingsStoreManager()
+    private let starBreakManager = StarBreakManager()
 
     func create(_ star: Star) {
         // 저장소에 star 생성
@@ -36,11 +37,15 @@ final class StarManager {
         deviceActivityScheduleManager.deleteSchedule(star)
         // star 블록 리스트 삭제
         managedSettingsStoreManager.endStar(star)
+        // star 중단 설정 삭제
+        starBreakManager.breakEnd(of: star)
     }
     
     func update(_ star: Star) {
         // 저장소의 star 업데이트
         userDefaultsManager.update(star)
+        // star 가 중단되어 있는 경우, 스케줄과 블록 리스트 업데이트 X
+        guard starBreakManager.breakEndTime(of: star) == nil else { return }
         // 등록된 star 의 스케줄 업데이트
         deviceActivityScheduleManager.updateSchedule(star)
         // star 의 상태를 통해 블록 리스트 업데이트
