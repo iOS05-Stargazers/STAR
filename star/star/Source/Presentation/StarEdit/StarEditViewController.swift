c59e1ad9aa6a5c46c023adac9c53eb016e86d416
name-mun <tains9153@gmail.com>
2025-04-14 11:08:42 +0900

    [Chores] 불필요한 코드 삭제

--- star/star/Source/Presentation/StarEdit/StarEditViewController.swift
+++ star/star/Source/Presentation/StarEdit/StarEditViewController.swift
@@ -322,7 +322,6 @@ extension StarEditViewController {
             set: { [weak self] newSelection in
                 self?.familyActivitySelection = newSelection
                 self?.viewModel.blockListRelay.accept(newSelection)
-                print("ddddd", newSelection)
                 hostingVC.dismiss(animated: true)
             }
         )
@@ -334,4 +333,3 @@ extension StarEditViewController {
         self.present(hostingVC, animated: true, completion: nil)
     }
 }
-// 스크린 타임 -> 접근 허용 토스트메세지
